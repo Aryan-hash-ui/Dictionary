cecb89645449bfb3400e7f34ce21380091d6a978
Aryan-hash-ui <76575896+Aryan-hash-ui@users.noreply.github.com>
2024-05-19 17:56:06 +0530

    Dictinary App

---
+++ script.js
@@ -0,0 +1,33 @@
+const url="https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result=document.getElementById("result");
+const sound=document.getElementById("sound");
+const btn=document.getElementById("search-btn");
+
+
+
+btn.addEventListener("click",()=>{
+    let inpWord=document.getElementById("inp-word").value
+    fetch(`${url}${inpWord}`)
+    .then((response)=>response.json())
+    .then((data)=>{
+        console.log(data);
+        result.innerHTML=`<div class="word">
+        <h3>${inpWord}</h3>
+        
+    </div>
+    <div class="details">
+        <p>${data[0].meanings[0].partOfSpeech}</p>
+        <p>/${data[0].phonetic}/</p>
+    </div>
+    <ol>
+        <li> <p class="word-meaning">${data[0].meanings[0].definitions[0].definition}</p></li>
+        <li><p class="word-meaning">${data[0].meanings[0].definitions[1].definition}</p></li>
+        <li><p class="word-meaning">${data[0].meanings[0].definitions[2].definition}</p></li>
+        <li><p class="word-meaning">${data[0].meanings[0].definitions[3].definition}</p></li>
+    </ol>
+   
+    
+    
+    `;
+    });
+})
